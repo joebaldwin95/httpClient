d0d6d08666f5014f3848a5bd70576426537b0999
Mike Conroy <mike.conroy@capgemini.com>
2015-09-03 14:30:50 +0100

    Adds HttpRequest Instance for calling the methods.

--- src/com/notpostman/main/NotPostmanUI.java
+++ src/com/notpostman/main/NotPostmanUI.java
@@ -22,6 +22,8 @@ public class NotPostmanUI {
 	private JTextField txtEnterUrlHere_1;
 	private JTextField txtEnterAnyParams;
 
+	private HttpRequest requester = new HttpRequest();
+
 	/**
 	 * Launch the application.
 	 */
@@ -54,57 +56,56 @@ public class NotPostmanUI {
 		frmNotpostmanV.setBounds(100, 100, 450, 290);
 		frmNotpostmanV.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmNotpostmanV.getContentPane().setLayout(null);
-		
+
 		txtEnterUrlHere_1 = new JTextField();
 		txtEnterUrlHere_1.setText("Enter URL Here");
 		txtEnterUrlHere_1.setBounds(6, 6, 438, 28);
 		frmNotpostmanV.getContentPane().add(txtEnterUrlHere_1);
 		txtEnterUrlHere_1.setColumns(10);
-		
+
 		JRadioButton rdbtnHttp = new JRadioButton("HTTP");
 		rdbtnHttp.setBounds(6, 68, 65, 23);
 		frmNotpostmanV.getContentPane().add(rdbtnHttp);
-		
+
 		JRadioButton rdbtnHttps = new JRadioButton("HTTPS");
 		rdbtnHttps.setBounds(78, 68, 72, 23);
 		frmNotpostmanV.getContentPane().add(rdbtnHttps);
-		
+
 		txtEnterAnyParams = new JTextField();
 		txtEnterAnyParams.setText("Enter Any Params Here");
 		txtEnterAnyParams.setBounds(6, 39, 438, 28);
 		frmNotpostmanV.getContentPane().add(txtEnterAnyParams);
 		txtEnterAnyParams.setColumns(10);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(159, 68, 103, 27);
 		frmNotpostmanV.getContentPane().add(comboBox);
 		comboBox.addItem("GET");
 		comboBox.addItem("POST");
-		
+
 		JButton btnGo = new JButton("GO");
 		btnGo.setBounds(274, 67, 117, 29);
 		frmNotpostmanV.getContentPane().add(btnGo);
-		
+
 		JTextArea textArea = new JTextArea();
 		textArea.setBounds(16, 103, 417, 151);
 		frmNotpostmanV.getContentPane().add(textArea);
-		
+
 		String method = (String) comboBox.getSelectedItem();
 		String formUrl = txtEnterUrlHere_1.getText();
 		String formParams = txtEnterAnyParams.getText();
 		Boolean httpRadio = rdbtnHttp.isSelected();
 		Boolean httpsRadio = rdbtnHttps.isSelected();
-		
+
 		btnGo.addActionListener(new ActionListener() {
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				
+			public void actionPerformed(ActionEvent e)
 				//Button action goes here
 				//Create request object
 				//Call appropriate method
 				//Set response into text area
-				
+
 			}
         });
 	}
